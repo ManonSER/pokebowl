b7fcdaa6f13696c1f0efe7a4d03d5bf9cd2ca02e
SER <m>
2021-05-19 15:26:24 +0200

    DAO - combat - tour - pokemonMatch

--- src/main/java/sopra/pokebowl/Application.java
+++ src/main/java/sopra/pokebowl/Application.java
@@ -3,10 +3,22 @@ package sopra.pokebowl;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import sopra.pokebowl.repository.ICombatRepository;
+import sopra.pokebowl.repository.IPokemonMatchRepository;
+import sopra.pokebowl.repository.ITourRepository;
+import sopra.pokebowl.repository.jpa.CombatRepositoryJpa;
+import sopra.pokebowl.repository.jpa.PokemonMatchRepositoryJpa;
+import sopra.pokebowl.repository.jpa.TourRepositoryJpa;
+
 public class Application {
 	private static Application instance = null;
 
 	private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("pokebowl");
+	
+	private final ICombatRepository combatRepo = new CombatRepositoryJpa();
+	private final ITourRepository tourRepo = new TourRepositoryJpa();
+	private final IPokemonMatchRepository pokemonMatchRepo = new PokemonMatchRepositoryJpa();
+	
 
 	private Application() {
 
@@ -23,5 +35,24 @@ public class Application {
 	public EntityManagerFactory getEmf() {
 		return emf;
 	}
+
+	public ICombatRepository getCombatRepo() {
+		return combatRepo;
+	}
+
+	public ITourRepository getTourRepo() {
+		return tourRepo;
+	}
+
+	public IPokemonMatchRepository getPokemonMatchRepo() {
+		return pokemonMatchRepo;
+	}
+
+	public static void setInstance(Application instance) {
+		Application.instance = instance;
+	}
+	
+	
+	
 
 }
