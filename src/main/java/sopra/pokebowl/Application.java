6e39a74210fe93f845611796dc6a4b83e6abe2d5
Lea Dumont <leadumont2512@gmail.com>
2021-05-19 15:25:16 +0200

    RepoJpa MonPoke+Equipe + maj

--- src/main/java/sopra/pokebowl/Application.java
+++ src/main/java/sopra/pokebowl/Application.java
@@ -3,10 +3,18 @@ package sopra.pokebowl;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import sopra.pokebowl.repository.IEquipeRepository;
+import sopra.pokebowl.repository.IMonPokemonRepository;
+import sopra.pokebowl.repository.jpa.EquipeRepositoryJpa;
+import sopra.pokebowl.repository.jpa.MonPokemonRepositoryJpa;
+
 public class Application {
 	private static Application instance = null;
 
 	private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("pokebowl");
+	
+	private final IMonPokemonRepository monPokeRepo = new MonPokemonRepositoryJpa();
+	private final IEquipeRepository equipeRepo = new EquipeRepositoryJpa();
 
 	private Application() {
 
@@ -22,6 +30,14 @@ public class Application {
 
 	public EntityManagerFactory getEmf() {
 		return emf;
+	}
+	
+	public IMonPokemonRepository getMonPokemonRepositoryJpa() {
+		return monPokeRepo;
+	}
+	
+	public IEquipeRepository getEquipeRepositoryJpa() {
+		return equipeRepo;
 	}
 
 }
---
+++ src/main/java/sopra/pokebowl/repository/jpa/EquipeRepositoryJpa.java
@@ -0,0 +1,78 @@
+package sopra.pokebowl.repository.jpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import sopra.pokebowl.Application;
+import sopra.pokebowl.model.Equipe;
+import sopra.pokebowl.repository.IEquipeRepository;
+
+public class EquipeRepositoryJpa implements IEquipeRepository {
+
+	@Override
+	public List<Equipe> findAll() {
+		List<Equipe> equipes = new ArrayList<Equipe>();
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Equipe> query = em.createQuery("select e from Equipe e", Equipe.class);
+
+			equipes = query.getResultList();
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return equipes;
+	}
+
+	@Override
+	public Equipe findById(Integer id) {
+		Equipe equipe = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			equipe = em.find(Equipe.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return equipe;
+	}
+
+}
