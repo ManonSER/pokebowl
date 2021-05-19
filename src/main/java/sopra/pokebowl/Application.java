652cf37b2034edf511fc2e3c4e38948db33f6acc
Florian <flo>
2021-05-19 15:24:06 +0200

    Interfaces et repo

--- src/main/java/sopra/pokebowl/Application.java
+++ src/main/java/sopra/pokebowl/Application.java
@@ -3,10 +3,21 @@ package sopra.pokebowl;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import sopra.pokebowl.repository.IAttaqueRepository;
+import sopra.pokebowl.repository.IPokemonRepository;
+import sopra.pokebowl.repository.ITypeClassRepository;
+import sopra.pokebowl.repository.jpa.AttaqueRepositoryJpa;
+import sopra.pokebowl.repository.jpa.PokemonRepositoryJpa;
+import sopra.pokebowl.repository.jpa.TypeClassRepositoryJpa;
+
 public class Application {
 	private static Application instance = null;
 
 	private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("pokebowl");
+	
+	private final IPokemonRepository pokemonRepo = new PokemonRepositoryJpa();
+	private final IAttaqueRepository attaqueRepo = new AttaqueRepositoryJpa();
+	private final ITypeClassRepository typeClassRepo = new TypeClassRepositoryJpa();
 
 	private Application() {
 
@@ -23,5 +34,11 @@ public class Application {
 	public EntityManagerFactory getEmf() {
 		return emf;
 	}
+
+	public IPokemonRepository getPokemonRepo() {
+		return pokemonRepo;
+	}
+	
+	
 
 }
