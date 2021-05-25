adf0b95c8e918ef780ab8f5cac319a909c9c1881
Lea <leakidrod@gmail.com>
2021-05-25 16:03:24 +0200

    maj import

--- src/main/java/sopra/pokebowl/repository/IAttaqueRepository.java
+++ src/main/java/sopra/pokebowl/repository/IAttaqueRepository.java
@@ -3,7 +3,6 @@ package sopra.pokebowl.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import sopra.pokebowl.model.Attaque;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IAttaqueRepository extends JpaRepository<Attaque, Long> {
 
--- src/main/java/sopra/pokebowl/repository/ITypeClassRepository.java
+++ src/main/java/sopra/pokebowl/repository/ITypeClassRepository.java
@@ -3,7 +3,6 @@ package sopra.pokebowl.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import sopra.pokebowl.model.TypeClass;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITypeClassRepository extends JpaRepository<TypeClass, Long>{
 
