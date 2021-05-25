c9f88be48b692867a482ccf0a7927c4707bd6639
Lea <leakidrod@gmail.com>
2021-05-25 14:58:12 +0200

    maj

--- src/main/java/sopra/pokebowl/repository/IEquipeRepository.java
+++ src/main/java/sopra/pokebowl/repository/IEquipeRepository.java
@@ -2,8 +2,16 @@ package sopra.pokebowl.repository;
 
 import java.util.List;
 
+import org.springframework.data.repository.query.Param;
+
 import sopra.pokebowl.model.Equipe;
 
-public interface IEquipeRepository extends IRepository<Equipe, Long> {
-	List<Equipe> findEquipesByUtilisateurId(Long id);
+public interface IEquipeRepository extends JpaRepository<Equipe, Long> {
+	
+	@Query("select e from Equipe e where e.utilisateurEquipeSauv.id = :idutil")
+	List<Equipe> findEquipesByUtilisateurId(@Param("idutil") Long id);
+	
+	List<Equipe> findAll();
+	
+	Equipe findById(Long id);
 }
--- src/main/java/sopra/pokebowl/repository/IMonPokemonRepository.java
+++ src/main/java/sopra/pokebowl/repository/IMonPokemonRepository.java
@@ -1,7 +1,11 @@
 package sopra.pokebowl.repository;
 
+import java.util.List;
+
 import sopra.pokebowl.model.MonPokemon;
 
 public interface IMonPokemonRepository extends IRepository<MonPokemon, Long> {
+	
+	List<MonPokemon> findAll();
 
 }
