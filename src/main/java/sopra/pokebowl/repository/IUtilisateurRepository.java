e4aa2cba12651a4933c8a328014b2ee7013acd8b
tristan <tris@g>
2021-06-09 14:19:41 +0200

    requete utilistauer

--- src/main/java/sopra/pokebowl/repository/IUtilisateurRepository.java
+++ src/main/java/sopra/pokebowl/repository/IUtilisateurRepository.java
@@ -1,6 +1,7 @@
 package sopra.pokebowl.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import sopra.pokebowl.model.Equipe;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,5 +16,8 @@ public interface IUtilisateurRepository extends JpaRepository<Utilisateur, Long>
 	
 	public Utilisateur findByPseudo(String pseudo);
 	
-	public Utilisateur findByEmail(String email);
+	public Utilisateur findByEmail(String email); 
+	
+	@Query("select distinct u from Utilisateur u left join fetch u.equipesSauvegardees where u.id = :id ")
+	public Optional<Utilisateur> findUtilisateurByIdWithEquipesSauvegardeesAndStatistique(@Param("id") Long id);
 }
